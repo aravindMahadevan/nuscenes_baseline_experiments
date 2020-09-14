f455b946ab02219292323b1eed7d6d16d8dd3992
aravindMahadevan <aravind.maha1397@gmail.com>
2020-09-14 11:55:33 -0700

    updated training

--- NuscenesDataset.py
+++ NuscenesDataset.py
@@ -4,41 +4,53 @@ from torch.utils.data import Dataset, DataLoader
 
 
 from nuscenes import NuScenes
+
 from nuscenes.eval.prediction.splits import get_prediction_challenge_split
 from nuscenes.eval.prediction.config import load_prediction_config
 from nuscenes.eval.prediction.splits import get_prediction_challenge_split
 from nuscenes.prediction import PredictHelper
+
 from nuscenes.prediction.models.physics import ConstantVelocityHeading, PhysicsOracle
 from nuscenes.prediction.input_representation.static_layers import StaticLayerRasterizer
 from nuscenes.prediction.input_representation.agents import AgentBoxesWithFadedHistory
 from nuscenes.prediction.input_representation.interface import InputRepresentation
 from nuscenes.prediction.input_representation.combinators import Rasterizer
 from PIL import Image
-import os
+import os 
+import random
 
 class NuscenesDataset(Dataset):
     
-    def __init__(self, NuscenesBaseDir, 
-                 data_set_version = 'v1.0-trainval', 
-                 maps_dir = 'maps',
+    def __init__(self, 
+                 nusc,
+                 helper,
+                 maps_dir,
                  save_maps_dataset = False, 
-                 split = 'train_val',
                  config_name = 'predict_2020_icra.json',
                  history=1, 
+                 num_examples = None,
                  in_agent_frame=True):
         
-        # path to main dataset
-        self.baseDir = NuscenesBaseDir
-        self.nusc = NuScenes(version=data_set_version, dataroot=self.baseDir, verbose=True)
-        self.helper = PredictHelper(self.nusc)
+        
+        self.nusc = nusc
+        self.helper = helper
+        
+        #initialize the data set 
+        if maps_dir == 'maps_train':
+            dataset_version = "train"
+        elif maps_dir == 'maps':
+            dataset_version = "train_val"
+        elif maps_dir == 'maps_val':
+            dataset_version = "val"
         
         #initialize maps directory where everything will be saved 
-        self.maps_dir = os.path.join(os.getcwd(), 'maps')
+        self.maps_dir = os.path.join(os.getcwd(), maps_dir)
+        self.data_set = get_prediction_challenge_split(dataset_version,dataroot=self.nusc.dataroot)
+        
+        if num_examples: 
+            self.data_set = self.data_set[:num_examples]
 
-        #initialize the data set 
-        self.data_set = get_prediction_challenge_split(split,dataroot=self.baseDir)
-
-        #initialize rasterizers for the 
+        #initialize rasterizers for map generation
         self.static_layer_rasterizer = StaticLayerRasterizer(self.helper)
         self.agent_rasterizer = AgentBoxesWithFadedHistory(self.helper, seconds_of_history=history)
         self.mtp_input_representation = InputRepresentation(self.static_layer_rasterizer, self.agent_rasterizer, Rasterizer())
@@ -56,6 +68,7 @@ class NuscenesDataset(Dataset):
             
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+            
     def save_maps(self):
         '''
         Input: None 
@@ -64,7 +77,7 @@ class NuscenesDataset(Dataset):
         This method finds all the valid data points in the data set. We define a valid data point 
         where the velocity, acceleartion, and heading specified by token not NaN. 
         '''
-        print("starting filtering and creation of data set")
+        print("starting to save maps")
         for i, token in enumerate(self.data_set): 
             instance_token_img, sample_token_img = self.data_set[i].split('_')
             
@@ -76,16 +89,17 @@ class NuscenesDataset(Dataset):
             im.save(file_path)
         
             print("{0}/{1} image saved".format(i, len(self.data_set)))
-            
-        print("done filtering data set ")
-       
+        
+        print("done saving maps")
+        
+        
+        
     def __len__(self):
         return len(self.data_set)
     
-    
     #return the image tensor, agent state vector, and the ground truth
     def __getitem__(self, index):
-        
+        token = self.data_set[index]
         instance_token_img, sample_token_img = self.data_set[index].split('_')
         
         velocity = self.helper.get_velocity_for_agent(instance_token_img, sample_token_img)
@@ -114,4 +128,4 @@ class NuscenesDataset(Dataset):
                                                         in_agent_frame=self.in_agent_frame)
         
         ground_truth = torch.Tensor(ground_truth).unsqueeze(0)
-        return image_tensor, agent_state_vec, ground_truth 
+        return image_tensor, agent_state_vec, ground_truth, token 
--- train.py
+++ train.py
@@ -1,6 +1,11 @@
-from nuscenes.prediction.models.backbone import ResNetBackbone
+from nuscenes import NuScenes
+from nuscenes.prediction import PredictHelper
+from nuscenes.prediction.models.backbone import ResNetBackbone, MobileNetBackbone
 from nuscenes.prediction.models.mtp import MTP, MTPLoss
-from nuscenes.prediction.models.covernet import CoverNet
+from nuscenes.prediction.models.covernet import CoverNet, ConstantLatticeLoss
+from nuscenes.eval.prediction.data_classes import Prediction
+
+
 import torch
 from torch.utils.data import DataLoader
 import torch.optim as optim
@@ -10,48 +15,94 @@ import time
 import os 
 import matplotlib
 import matplotlib.pyplot as plt 
+import pickle
+import random
+import json
 
-class MTP_Experiment():
+class Nuscenes_Baseline_Experiment():
     def __init__(self, 
-                 M = 20, 
-                 num_epochs = 50, 
+                 output_dir,
+                 data_version = 'v1.0-trainval',
+                 M = 16, 
+                 num_epochs = 100, 
                  num_workers = 4,
-                 output_dir = 'mtp_experiment_data',
-                 lr = 3e-4,
-                 batch_size=8, 
+                 num_training_examples = None,
+                 num_validation_examples = None, 
+                 model = 'MTP',
+                 lr = 1e-4,
+                 batch_size=16, 
+                 training_maps = 'maps_train',
+                 validation_maps = 'maps_val',
                  data_root = '../full_data/sets/nuscenes',
-                 backbone_architecture='resnet50'):
+                 trajectory_set_path = None, 
+                 backbone_architecture='mobilenet_v2'):
         
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         print(self.device)
+        
         #init experiment parameters
         self.output_dir = output_dir
         self.checkpoint_path = os.path.join(self.output_dir, "checkpoint.pth.tar")
         self.config_path = os.path.join(self.output_dir, "config.txt")
-        
+                
         self.num_epochs = num_epochs
         self.batch_size = batch_size 
         self.lr = lr 
-        self.backbone_architecture = 'resnet50'
+        self.backbone_architecture = backbone_architecture
         
-        #init network 
+        #init network and criterion
         self.num_modes = M
-        self.backbone = ResNetBackbone(backbone_architecture)
-        self.mtp = MTP(self.backbone, num_modes=self.num_modes).to(self.device) 
+        self.backbone = MobileNetBackbone(backbone_architecture)
+        
+        if model == 'MTP':
+            self.model = MTP(self.backbone, num_modes=self.num_modes).to(self.device) 
+            self.criterion = MTPLoss(self.num_modes)
+        elif model == 'CoverNet':
+            self.model = CoverNet(self.backbone, num_modes=self.num_modes).to(self.device)
+            if not trajectory_set_path or not os.path.isfile(trajectory_set_path): 
+                raise ValueError("Invalid trajectory set path for CoverNet. Check path.")
+            trajectories =pickle.load(open(trajectory_set_path, 'rb'))
+            trajectories = torch.Tensor(trajectories)
+            self.criterion = ConstantLatticeLoss(trajectories).to(self.device)
+        else:
+            raise ValueError("Invalid model specification")
+            
+           
         
         #init data set and data loader 
-        self.dataset = NuscenesDataset(data_root)
-        self.dataloader = DataLoader(self.dataset, 
-                                     batch_size=self.batch_size, 
-                                     shuffle = True, 
-                                     pin_memory = True,
-                                     num_workers = num_workers)
+        self.nusc = NuScenes(version=data_version, dataroot=data_root, verbose=True)
+        self.helper = PredictHelper(self.nusc)
         
-        #init loss function and optimizers
-        self.criterion = MTPLoss(self.num_modes)
-        self.optimizer = optim.Adam(self.mtp.parameters(), lr=self.lr)    
+        self.training_set = NuscenesDataset(self.nusc, 
+                                            self.helper, 
+                                            maps_dir = training_maps, 
+                                            num_examples = num_training_examples)
+        
+        self.validation_set = NuscenesDataset(self.nusc, 
+                                             self.helper, 
+                                             maps_dir = validation_maps, 
+                                             num_examples = num_validation_examples)      
+        
+        print(len(self.validation_set)) 
+        print(len(self.training_set))
+        self.train_loader = DataLoader(self.training_set, 
+                                       batch_size=self.batch_size, 
+                                       shuffle = True,
+                                       pin_memory = True,
+                                       num_workers = num_workers)
+        
+        
+        self.val_loader = DataLoader(self.validation_set, 
+                                     batch_size=self.batch_size, 
+                                     shuffle = True,
+                                     pin_memory = True,
+                                     num_workers = num_workers)        
+        
+        self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr)    
         
         self.train_loss = []
+        self.val_loss = []
+        self.val_epochs = []
         
         #check if already created experiment
         if os.path.isfile(self.config_path):
@@ -67,7 +118,7 @@ class MTP_Experiment():
         return len(self.train_loss)
 
     def setting(self):
-        return {'MTP' : self.mtp,
+        return {'Model' : self.model,
                 'Optimizer' : self.optimizer,
                 'Number of Modes' : self.num_modes,
                 'Backbone architecture' : self.backbone_architecture, 
@@ -82,19 +133,23 @@ class MTP_Experiment():
         return string
 
     def state_dict(self):
-        return {'MTP' : self.mtp.state_dict(),
+        return {'Model' : self.model.state_dict(),
                 'Optimizer' : self.optimizer.state_dict(),
-                'TrainLoss' : self.train_loss}
+                'TrainLoss' : self.train_loss, 
+                'ValLoss': self.val_loss, 
+                'ValEpochs': self.val_epochs}
         
     def save_fig(self):
         fig, ax = plt.subplots(1)
         ax.plot(range(len(self.train_loss)), self.train_loss, 'r', label='Train Loss')
+        ax.plot(self.val_epochs, self.val_loss, 'b', label='Validation Loss')
         ax.legend()
         plt.ylabel('Loss')
         plt.xlabel('Epochs')
-        fig.savefig(os.path.join(self.output_dir, 'mtp_loss_epoch'))
+        plt.title('MTP Training and Validation Loss')
+        fig.savefig(os.path.join(self.output_dir, 'mtp_train_loss_epoch'))
         plt.close()
-
+        
     def save(self):
         self.save_fig()
         torch.save(self.state_dict(), self.checkpoint_path)
@@ -102,9 +157,11 @@ class MTP_Experiment():
             print(self, file=f)
 
     def load_state_dict(self, checkpoint):
-        self.mtp.load_state_dict(checkpoint['MTP'])
+        self.model.load_state_dict(checkpoint['Model'])
         self.optimizer.load_state_dict(checkpoint['Optimizer'])
         self.train_loss = checkpoint['TrainLoss']
+        self.val_loss = checkpoint['ValLoss']
+        self.val_epochs = checkpoint['ValEpochs']
         
         for state in self.optimizer.state.values():
             for k, v in state.items():
@@ -117,36 +174,93 @@ class MTP_Experiment():
         del checkpoint
     
     def run(self):
-        print("Starting/Continuing Training!")
-        for epoch in range(self.num_epochs): 
+        
+        starting_epoch = len(self.train_loss)
+        print("Starting/Continuing Training at epoch {0}!".format(starting_epoch))
+        for epoch in range(starting_epoch, self.num_epochs): 
             self.train(epoch)
             
-            if epoch % 10 == 0: 
-                print('saving weights')
-                self.save()
-                
+#             if epoch % 10 == 0: 
+            self.val_epochs.append(epoch)
+            print('calculating validation loss')
+            self.compute_validation_loss(epoch)
+            print('saving weights')
+            self.save()
+
+#        self.val_epochs.append(epoch)
+#        print("computing final validation loss")
+#        self.compute_validation_loss(epoch)
+#        print('saving final weights')
         self.save()
         print("Done training!")
+
+    def compute_validation_loss(self, epoch): 
+        epoch_loss = 0
+        count=0
+        with torch.no_grad():
+            for image_tensor, agent_vec, ground_truth, _ in self.val_loader:
+                output = self.model(image_tensor.to(self.device), agent_vec.to(self.device))
+                loss = self.criterion(output, ground_truth.to(self.device))
+                count+=1
+                print("{0}/{1} validation batches finished".format(count, len(self.val_loader)))
+                epoch_loss += loss.item()
+
+        average_loss = epoch_loss/(len(self.val_loader))
+        print("Epoch Validation {0}/{1} complete. Loss at epoch: {2}".format(epoch, self.num_epochs - 1, average_loss))
+        print("Ending Validation Epoch {0} at {1}".format(epoch, str(time.strftime("%H:%M:%S", time.localtime()))))
+        self.val_loss.append(average_loss)     
           
     def train(self, epoch): 
         epoch_loss = 0
         count=0
-        
         print("Starting Epoch {0} at {1}".format(epoch, str(time.strftime("%H:%M:%S", time.localtime()))))
-        for image_tensor, agent_vec, ground_truth in self.dataloader:
-            output = self.mtp(image_tensor.to(self.device), agent_vec.to(self.device))
+        for image_tensor, agent_vec, ground_truth, _ in self.train_loader:
+            output = self.model(image_tensor.to(self.device), agent_vec.to(self.device))
             loss = self.criterion(output, ground_truth.to(self.device))
             self.optimizer.zero_grad()
             loss.backward()
             self.optimizer.step()
             count+=1
-            print("{0}/{1} batches finished".format(count, len(self.dataloader)))
-            epoch_loss += loss
+            print("{0}/{1} training batches finished".format(count, len(self.train_loader)))
+            epoch_loss += loss.item()
             
-        print("Epoch {0}/{1} complete. Loss at epoch: {2}".format(epoch, self.num_epochs - 1, epoch_loss))
+        average_loss = epoch_loss/(len(self.train_loader))
+        print("Epoch {0}/{1} complete. Loss at epoch: {2}".format(epoch, self.num_epochs - 1, average_loss))
         print("Ending Epoch {0} at {1}".format(epoch, str(time.strftime("%H:%M:%S", time.localtime()))))
-        self.train_loss.append(epoch_loss)
+        self.train_loss.append(average_loss)
 
+    def generate_predictions_from_validation(self, json_file_name = 'mtp_preds.json'): 
+        mtp_output = []
+        val_loader = DataLoader(self.validation_set, 
+                                     batch_size=1, 
+                                     shuffle = True,
+                                     pin_memory = True,
+                                     num_workers = 0)  
+        
+        print('starting validation predictions')
+        with torch.no_grad():
+            count = 0
+            for image_tensor, agent_state_vec, ground_truth, token  in val_loader:
+                config = self.validation_set.config
+                instance_token_img, sample_token_img = token[0].split('_')
+                
+                output = self.model(image_tensor.to(self.device), agent_state_vec.to(self.device))
+                prediction = output[:,:-self.num_modes].cpu().numpy()
+                probabilites = output[:,-self.num_modes:].squeeze(0).cpu().numpy()
+                prediction = prediction.reshape(self.num_modes, config.seconds * 2, 2)
+
+                serialized_pred = Prediction(instance_token_img, sample_token_img, prediction, probabilites).serialize()
+                mtp_output.append(serialized_pred)
+                print("{0}/{1} predictions saved".format(count, len(val_loader)))
+                count+=1
+                    
+                             
+        print("saving predictions")
+        json.dump(mtp_output, open(json_file_name, "w"))
+            
+            
 if __name__ == "__main__":
-    exp = MTP_Experiment(output_dir = 'exp4')
+    exp = Nuscenes_Baseline_Experiment(output_dir = 'exp13', model = 'MTP')
     exp.run()
+    exp.generate_predictions_from_validation()
+
